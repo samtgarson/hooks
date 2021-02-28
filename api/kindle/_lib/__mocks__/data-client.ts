3090a9e8b3fc78729b3f5e2b30cc217397329757
Sam Garson <samtgarson@gmail.com>
2021-02-28 11:17:44 +0000

    Remove kindle hook

--- api/kindle/_lib/__mocks__/data-client.ts
+++
@@ -1,9 +0,0 @@
-export const createArticleMock = jest.fn()
-export const getUnprocessedArticlesMock = jest.fn()
-export const destroyProcessedArticlesMock = jest.fn()
-
-export const DataClient = jest.fn(() => ({
-	createArticle: createArticleMock,
-	getUnprocessedArticles: getUnprocessedArticlesMock,
-	destroyProcessedArticles: destroyProcessedArticlesMock
-}))
